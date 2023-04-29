f4340ec88fe732893d427528cd993f157703fad4
hh2204 <132136150+hh2204@users.noreply.github.com>
2023-04-29 18:14:09 +0700

    Add files via upload

---
+++ task 7.h
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+struct student
+{
+    char ID[10];
+    char name[30];
+    char DoB[11];
+    char lname[10];
+};
+struct student student[10] = {
+    {"22BI13197", "Nguyen Quang Huy", "07/05/1999"},
+    {"22BI13471", "Do Duc Vinh", "15/03/2004"},
+    {"22BI13432", "Nguyen Ngoc Thanh Truc", "10/08/2004"},
+    {"BA12-019", "Nguyen Ngoc Anh", "09/04/2003"},
+    {"22BI13414", "Nguyen Phuong Thao", "23/02/2004"},
+    {"22BI13362", "Duong Thu Phuong", "21/09/2004"},
+    {"22BI13083", "Vu Gia Dat", "01/11/2004"},
+    {"22BI13176", "Tran Huy Hoang", "17/08/2004"},
+    {"22BI13131", "Nguyen Truong Giang", "20/06/2004"},
+    {"BA12-016", "Nguyen Nu Lan Anh", "03/02/2003"},
+};
